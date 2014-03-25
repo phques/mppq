1e63bd6f1f76d20a2662fb86004e84460437d2f4
Philippe Quesnel <philippe.quesnel@gmail.com>
2014-03-25 12:27:28 -0400

    started to include UDP part ..

--- jsonrpc/src/testServer/jsonRpcServer.go
+++ jsonrpc/src/testServer/jsonRpcServer.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"arith"
-	//"fmt"
+	"fmt"
 	"log"
 	"net"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
 
-func startServerOrig() {
+func startJSONServerOrig() {
 	arith := new(arith.Arith)
 
 	server := rpc.NewServer()
@@ -32,7 +32,7 @@ func startServerOrig() {
 }
 
 // simpler version
-func startServer() {
+func startJSONServer() {
 
 	rpc.Register(new(arith.Arith))
 
@@ -41,6 +41,7 @@ func startServer() {
 		log.Fatal("listen error:", e)
 	}
 	defer l.Close()
+	fmt.Printf("listener address: %v\n", l.Addr().String())
 
 	for {
 		conn, err := l.Accept()
@@ -52,6 +53,39 @@ func startServer() {
 	}
 }
 
+func listenUDP() {
+	// --- listen for udp msg ---
+	udpConn, err := net.ListenUDP("udp4", &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 4444,
+	})
+	if err != nil {
+		log.Fatal("error open udp socket ", err)
+	}
+	defer udpConn.Close()
+
+	// wait for msg
+	data := make([]byte, 1024*2)
+	for {
+		fmt.Println("wait for UDP msg")
+		nbRead, remoteAddr, err := udpConn.ReadFromUDP(data)
+		if err != nil {
+			log.Fatal("error reading udp socket ", err)
+		}
+		udpData := data[:nbRead]
+
+		fmt.Printf("read %d bytes '%s' from udp (remote %s)\n", nbRead, udpData, remoteAddr.String())
+		if string(udpData) == "quit" {
+			break
+		}
+
+		// did we get a mppq.whosthere for androidpush or * ?
+
+		//fmt.Println("wait a sec...")
+		//time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	startServer()
+	startJSONServer()
 }
