5e25385e948fe0276defb8e19e4f6af70ebabbc1
Philippe Quesnel <philippe.quesnel@gmail.com>
2024-08-03 19:08:43 -0400

    completed the example by providing an rpc service

--- examples/client/client.go
+++ examples/client/client.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/rpc"
 
 	"github.com/phques/mppq"
 )
 
+type RpcArgs struct {
+	Arg string
+}
+type RpcReply struct {
+	Reply string
+}
+
 func main() {
+	// query for provided services (want rpc server)
 	println("starting mppq client... for 10 seconds...")
 	mppqResponse, err := mppq.QueryServiceDefs(10)
 
@@ -20,4 +29,31 @@ func main() {
 		}
 	}
 
+	// get rpc server info from service def (we expect only one)
+	serviceDef := mppqResponse.ServiceDefs[0]
+	if serviceDef.ServiceName != "RpcServerExample" {
+		log.Fatal("expected service name 'RpcServerExample', got: ", serviceDef.ServiceName)
+	}
+	rpcServer := serviceDef.HostIP + ":" + fmt.Sprintf("%d", serviceDef.HostPort)
+
+	// create rpc client
+	client, err := rpc.DialHTTP("tcp", rpcServer)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	// call rpc server
+	println("\n\ncalling rpc server @", rpcServer, "(info from mppqResponse)")
+
+	args := &RpcArgs{Arg: "Phil"}
+	rpcReply := &RpcReply{Reply: "reply"}
+
+	err = client.Call("RpcServerExample.Test", args, &rpcReply)
+
+	// show results
+	if err != nil {
+		log.Fatal("arith error:", err)
+	} else {
+		println("RpcServerExample, received reply:", rpcReply.Reply)
+	}
 }
--- examples/server/server.go
+++ examples/server/server.go
@@ -2,27 +2,54 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
 	"time"
 
 	"github.com/phques/mppq"
 )
 
+type RpcServerExample struct {
+	data int
+}
+
+type RpcArgs struct {
+	Arg string
+}
+type RpcReply struct {
+	Reply string
+}
+
+func (s *RpcServerExample) Test(args RpcArgs, reply *RpcReply) error {
+	println("RpcServerExample, received request:", args.Arg, ", s.data:", s.data)
+	reply.Reply = "hello " + args.Arg
+	return nil
+}
+
 func main() {
+	// start an rpc server for RpcServerExample
+	println("starting rpc server...")
+	rpcServerExample := &RpcServerExample{data: 12}
+	rpc.Register(rpcServerExample)
+	rpc.HandleHTTP()
+	l, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+	go http.Serve(l, nil)
+
+	// create service def for rpc server
 	serviceDefs := []mppq.ServiceDef{
-		mppq.NewLocalServiceDef("marco", 1440, "jsonrpcv1"),
-		mppq.NewLocalServiceDef("polo", 1441, "jsonrpcv1"),
-		mppq.NewLocalServiceDef("pippo", 1442, "jsonrpcv1"),
-		mppq.NewLocalServiceDef("pluto", 1443, "jsonrpcv1"),
-		mppq.NewLocalServiceDef("paperino", 1444, "jsonrpcv1"),
-		mppq.NewLocalServiceDef("topolino", 1445, "jsonrpcv1"),
+		mppq.NewLocalServiceDef("RpcServerExample", 1234, "http rpc"),
 	}
 
-	// start mppq server
-	ctx, cancel := context.WithCancel(context.Background())
-
-	mppqServer := mppq.NewMppqServer(serviceDefs)
-
+	// start mppq server for 30 seconds
 	println("starting mppq server, for 30 seconds...")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	mppqServer := mppq.NewMppqServer(serviceDefs)
 	go mppqServer.Serve(ctx)
 
 	time.Sleep(60 * time.Second)
